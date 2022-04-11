6cb249aa1563ae6880bcb1581c3cce2e79ca25bb
Samuel <torressam333@gmail.com>
2022-04-11 07:21:50 -0700

    WIP scaffolding register component

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,6 +5,7 @@ import {
   faInfoCircle
 } from "@fortawesome/free-solid-svg-icons";
 import "../assets/register.css";
+import { authRegex } from "../util/authRegex";
 
 const Register = () => {
   //Set focus on user input when comp loads
@@ -12,50 +13,50 @@ const Register = () => {
   //If err, put focus on error
   const errRef = useRef();
 
-  //Condensed state
-  const [state, setState] = useState({
-    errMsg: "",
-    success: false
-  });
+  //User field initial state
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  //PW field initial state
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  //Matching pw initial state
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  //Screen msg's initial state
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
 
   //Set focus when comp loads
-  // useEffect(() => {
-  //   userRef.current.focus();
-  // }, []);
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  //Validate username against regex with input changes
+  useEffect(() => {
+    setValidName(authRegex.userRegex.test(user));
+  }, [user]);
 
   return (
     <section>
       <p
         ref={errRef}
-        className={state.errMsg ? "errmsg" : "offscreen"}
+        className={errMsg ? "errmsg" : "offscreen"}
         aria-live="assertive"
       >
-        {state.errMsg}
+        {errMsg}
       </p>
       <h1>Sign Up</h1>
+      <form>
+        <input />
+      </form>
     </section>
   );
 };
 
 export default Register;
-
-//May use this later
-
-// //User field initial state
-// const [user, setUser] = useState("");
-// const [validName, setValidName] = useState(false);
-// const [userFocus, setUserFocus] = useState(false);
-
-// //PW field initial state
-// const [pwd, setPwd] = useState("");
-// const [validPwd, setValidPwd] = useState(false);
-// const [pwdFocus, setPwdFocus] = useState(false);
-
-// //Matching pw initial state
-// const [matchPwd, setMatchPwd] = useState("");
-// const [validMatch, setValidMatch] = useState(false);
-// const [matchFocus, setMatchFocus] = useState(false);
-
-// //Screen msg's initial state
-// const [errMsg, setErrMsg] = useState("");
-// const [success, setSuccess] = useState(false);
